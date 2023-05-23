1399882c3103677963c5d5d99764412f75103a53
Caio Barilli <caiobarilli@gmail.com>
2023-05-23 07:21:15 -0300

    [app] feature: add eslint & prettier

---
+++ app/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ["@react-native-community", "eslint-config-prettier"],
+  rules: {
+    "react-native/no-inline-styles": 0, // Disable inline styles
+    "prettier/prettier": "error", // Prettier rules
+    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+    "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
+    "react/prop-types": "off", // Provide types statically using TypeScript
+    "react/react-in-jsx-scope": "off", // Disallow missing React when using JSX
+    "@typescript-eslint/explicit-module-boundary-types": "off", //Explicit types for function return values
+    "@typescript-eslint/no-non-null-assertion": "off", // does not use the strictNullChecks compiler option
+    "@typescript-eslint/no-unused-vars": "warn", // Unused variables
+  },
+};
--- app/App.tsx
+++ app/App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
@@ -11,9 +11,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    color: '#fff',
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
