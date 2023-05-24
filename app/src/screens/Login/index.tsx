d501946fdb4832fb0f4c8662471adbdf0d4e3402
Caio Barilli <caiobarilli@gmail.com>
2023-05-23 21:17:46 -0300

    [app] feature: update component structure

---
+++ app/src/screens/Login/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./styles";
+
+export default () => (
+  <S.Container>
+    <S.Title>Login</S.Title>
+  </S.Container>
+);
--- app/src/screens/Login/styles.tsx
+++ app/src/screens/Login/styles.tsx
@@ -1,21 +1,14 @@
-import React from "react";
 import styled from "styled-components/native";
 
-const Container = styled.View`
+export const Container = styled.View`
   flex: 1;
   background-color: #000;
   align-items: center;
   justify-content: center;
 `;
 
-const Title = styled.Text`
+export const Title = styled.Text`
   color: #fff;
   text-align: center;
   font-size: 32px;
 `;
-
-export default () => (
-  <Container>
-    <Title>Hello</Title>
-  </Container>
-);
