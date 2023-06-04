d7775f1bf63ef0037490c0d7dbc3aeab4383265a
Caio Barilli <caiobarilli@gmail.com>
2023-06-04 18:02:03 -0300

    feature: update screens

--- app/src/screens/Register/index.tsx
+++ app/src/screens/Register/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Title from "../../components/Title";
+import Text from "../../components/Text";
+import Input from "../../components/Input";
 import Button from "../../components/Button";
 import * as S from "./styles";
 
@@ -10,8 +12,15 @@ interface RegisterProps {
 const Register: React.FC<any> = ({ navigation }: RegisterProps) => {
   return (
     <S.Container>
-      <Title>Register</Title>
-      <Button title="Cancel" onPress={() => navigation.navigate("Home")} />
+      <S.FormContainer>
+        <Title>Register</Title>
+        <Input placeholder="Name" />
+        <Input placeholder="Lastname" />
+        <Input placeholder="E-mail" keyboardType="email-address" />
+        <Input placeholder="Password" secureTextEntry={true} />
+        <Input placeholder="Confirm Password" secureTextEntry={true} />
+        <Button title="Register" onPress={() => navigation.navigate("Home")} />
+      </S.FormContainer>
     </S.Container>
   );
 };
--- app/src/screens/Register/styles.tsx
+++ app/src/screens/Register/styles.tsx
@@ -2,4 +2,22 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const FormContainer = styled.View`
+  background-color: #e7e7e7;
+  padding: 20px;
+  border-radius: 10px;
+  width: 80%;
+`;
+
+export const RegisterWrapper = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  padding: 0;
+  width: 80%;
+  margin-top: 20px;
 `;
