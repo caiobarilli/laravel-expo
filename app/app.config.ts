d59d57f2989833eda6088db9479cf7b444149eae
Caio Barilli <caiobarilli@gmail.com>
2023-06-12 20:50:08 -0300

    Update expo config

--- app/app.config.ts
+++ app/app.config.ts
@@ -2,13 +2,31 @@ import { ExpoConfig, ConfigContext } from "expo/config";
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  slug: "my-app",
   name: "My App",
+  slug: "my-app",
+  version: "1.0.0",
+  orientation: "portrait",
   icon: "./src/assets/icon.png",
   splash: {
     image:
       "https://github.com/expo/expo/blob/master/templates/expo-template-blank/assets/splash.png?raw=true",
     resizeMode: "contain",
     backgroundColor: "#ffffff",
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./src/assets/adaptive-icon.png",
+      backgroundColor: "#FFFFFF",
+    },
+  },
+  web: {
+    favicon: "./src/assets/favicon.png",
   },
 });
